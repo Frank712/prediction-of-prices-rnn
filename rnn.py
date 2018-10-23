a0841a596c165e0670886f6178b114e96bc84a0b
Frank712 <franksuriel_7@hotmail.com>
2018-10-22 23:15:28 -0500

    Building the RNN, adding four LTSM layers and Dropout

--- rnn.py
+++ rnn.py
@@ -3,7 +3,7 @@
 """
 Created on Tue Oct  9 03:38:29 2018
 
-@author: frank7
+@author: 
 """
 # Recurrent Neural Network
 
@@ -16,17 +16,63 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the training set
-
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values
 
 #Feature Scaling
-
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range=(0, 1))
 
 training_set_scaled = sc.fit_transform(training_set)
 
 #Creating a data structure with 60 steps and 1 output
+X_train = []
+Y_train = []
 
+for i in range(60,1258):
+    X_train.append(training_set_scaled[i-60: i, 0])
+    Y_train.append(training_set_scaled[i, 0])
+X_train, Y_train = np.array(X_train), np.array(Y_train)
+
+# Reshaping 
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+#Part #2 Building the RNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+# Initializing the RNN
+regressor = Sequential()
+
+# Adding the first LSTM layer and some Dropout regularisation
+regressor.add(LSTM( units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+# Adding the second layer and some Dropout regularisation
+regressor.add(LSTM( units=50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# Adding the third layer and some Dropout regularisation
+regressor.add(LSTM( units=50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# Adding the fourth layer and some Dropout regularisation
+regressor.add(LSTM( units=50))
+regressor.add(Dropout(0.2))
+
+
+
+
+
+
+
+
+
+
+
+           
 
